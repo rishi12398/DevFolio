eb83adaa2afd7a1d1b4036e294661999238da013
rishi12398 <rishi12398@gmail.com>
2022-08-24 21:07:42 +0530

    Swiper: Testimonials Section Added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import "./testimonials.css"
+import Comments from './Comments'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className="testimonials container section">
+      <h2 className="section__title">Testimonials</h2>
+
+      <Swiper className="testimonials__container grid"
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        loop={true}
+        grabCursor={true}
+      >
+        {Comments.map(({id, image, title, subtitle, comment}) => {
+          return (
+            <SwiperSlide className="testimonials__item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testimonials__title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </section>
   )
 }
 
