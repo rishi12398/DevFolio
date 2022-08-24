5464ce5c17dfc9bc3384d38aebb34a0a34e9d546
rishi12398 <rishi12398@gmail.com>
2022-08-24 21:30:24 +0530

    Blogs/Articles Section Added

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import "./blog.css"
+import Image1 from "../../assets/blog-1.svg"
+import Image2 from "../../assets/blog-2.svg"
+import Image3 from "../../assets/blog-3.svg"
+
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog container section" id="blog">
+      <h2 className="section__title">Latest Posts</h2>
+
+      <div className="blog__container grid">
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="#"><span className="blog__category">Reviews</span></a>
+            <a href="#"><img src={Image1} alt="" className="blog__img" /></a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">What to write here then?</h3>
+            <div className="blog__meta">
+              <span>24th August 2022</span>
+              <span className="blog__dot">.</span>
+              <span>BOLBY??</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="#"><span className="blog__category">Tutorials</span></a>
+            <a href="#"><img src={Image2} alt="" className="blog__img" /></a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">Something Cool Maybe?</h3>
+            <div className="blog__meta">
+              <span>24th August 2022</span>
+              <span className="blog__dot">.</span>
+              <span>BOLBY??</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="#"><span className="blog__category">Explosions</span></a>
+            <a href="#"><img src={Image3} alt="" className="blog__img" /></a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">Well that would be misleading XD.</h3>
+            <div className="blog__meta">
+              <span>24th August 2022</span>
+              <span className="blog__dot">.</span>
+              <span>BOLBY??</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
