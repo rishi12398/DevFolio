f5b664f03b706fef2a96e91327a909498a89ae94
rishi12398 <rishi12398@gmail.com>
2022-08-24 23:59:51 +0530

    Folio Mobile Ready

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./sidebar.css"
 import Logo from "../../assets/logo.svg"
 const Sidebar = () => {
+    const [toggle, showMenu] = useState(false);
   return (
     <>
-    <aside className='aside'>
+    <aside className={toggle ? "aside show-menu" : "aside"}>
         <a href="#home" className="nav__logo">
             <img src={Logo} alt=""/>
         </a>
@@ -58,7 +59,8 @@ const Sidebar = () => {
         </div>
     </aside>
 
-    <div className="nav__toggle">
+    <div className={toggle ? "nav__toggle nav__toggle-open" :
+    "nav__toggle"} onClick={() => showMenu(!toggle)}>
         <i className="icon-menu"></i>
     </div>
     </>
